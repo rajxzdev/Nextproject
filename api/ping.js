c8cadfe987b730a99674123a0068704bd20d34ff
rajxzdev <rajxz8273@gmail.com>
2026-02-15 14:24:38 +0700

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,7 @@
-module.exports = function(req, res) {
-    res.json({ ok: true });
+module.exports = function (req, res) {
+    res.status(200).json({
+        ok: true,
+        message: "Server is alive!",
+        time: new Date().toISOString()
+    });
 };
